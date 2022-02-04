c1e27dbe9d32cd2a653281edca0ce8ab2179cb12
Franco <francoezapata1@gmail.com>
2022-02-04 00:57:02 -0300

    First changes

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,28 +59,23 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Franco Zapata.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I am a tech enthusiast with large experience in business and coordinating work groups that
+        now is looking for a job as a backend developer using Ruby On Rails as main technology.
       </p>
     </>
   );
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="mailto:francoezapata1@gmail.com"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Get in touch
     </a>
   );
 
